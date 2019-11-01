ff03cdb1f00c6dd573af76edb28397c74d82f32b
Ryan <blackielifegfgaming@gmail.com>
2019-11-01 09:52:55 +0000

    Changes to frontend api calling
    Api calls are now intercepted and attachs the authorizatrion header

--- frontend/src/apis/api.js
+++ frontend/src/apis/api.js
@@ -1,27 +1,33 @@
 import axios from 'axios';
 
-const apiConfig = {
-    headers: {
-        Authorization: localStorage.getItem('token') ? localStorage.getItem('token') : "none"
-    }
-};
-
 const baseApi = axios.create({
         baseURL: "http://localhost:8080/api"
 });
 
+baseApi.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token') ? localStorage.getItem('token') : "none";
+
+    if ( token != null ) {
+        config.headers.Authorization = token;
+    }
+
+    return config;
+}, (err) => {
+    return Promise.reject(err);
+});
+
 export const apiGetCall = async endpoint => {
-    return await baseApi.get(endpoint, apiConfig);
+    return await baseApi.get(endpoint);
 };
 
 export const apiPostCall = async (endpoint, data) => {
-    return await baseApi.post(endpoint, data, apiConfig);
+    return await baseApi.post(endpoint, data);
 };
 
 export const apiPutCall = async (endpoint, data) => {
-    return await baseApi.put(endpoint, data, apiConfig);
+    return await baseApi.put(endpoint, data);
 };
 
 export const apiDeleteCall = async (endpoint) => {
-    return await baseApi.delete(endpoint, apiConfig);
+    return await baseApi.delete(endpoint);
 };
