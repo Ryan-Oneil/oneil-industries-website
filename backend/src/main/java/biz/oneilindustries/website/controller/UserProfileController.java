872f41e1c54f994da4831c11d7dd1195c97bae70
Ryan <blackielifegfgaming@gmail.com>
2019-11-16 13:48:06 +0000

    API changes
    Moved service related apis from profile controller to the services controller  for consistency

--- backend/src/main/java/biz/oneilindustries/website/controller/UserProfileController.java
+++ backend/src/main/java/biz/oneilindustries/website/controller/UserProfileController.java
@@ -1,22 +1,14 @@
 package biz.oneilindustries.website.controller;
 
-import biz.oneilindustries.website.entity.DiscordUser;
-import biz.oneilindustries.website.entity.TeamspeakUser;
 import biz.oneilindustries.website.entity.User;
-import biz.oneilindustries.website.pojo.ServiceClient;
-import biz.oneilindustries.website.service.ManagerService;
 import biz.oneilindustries.website.service.UserService;
 import biz.oneilindustries.website.validation.UpdatedUser;
 import java.util.HashMap;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,15 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class UserProfileController {
 
-    private static final String ADMIN_ROLE = "ROLE_ADMIN";
-
     private final UserService userService;
-    private final ManagerService managerService;
 
     @Autowired
-    public UserProfileController(UserService userService, ManagerService managerService) {
+    public UserProfileController(UserService userService) {
         this.userService = userService;
-        this.managerService = managerService;
     }
 
     @GetMapping("/profile")
@@ -70,50 +58,5 @@ public class UserProfileController {
         return ResponseEntity.ok("Successfully updated account details");
     }
 
-    @PostMapping("/profile/addservice/{service}")
-    public ResponseEntity addUserService(@PathVariable String service, @RequestBody ServiceClient serviceClient, Authentication user, HttpServletRequest request) {
 
-        if (request.isUserInRole("ROLE_UNREGISTERED")) {
-            ResponseEntity.status(HttpStatus.FORBIDDEN).body("Your account must be approved to register a service");
-        }
-
-        // Returns the hibernate entity as response for frontend consumption
-        if (service.equalsIgnoreCase("teamspeak")) {
-            return ResponseEntity.ok(managerService.addTeamspeakService(user.getName(), serviceClient));
-        }else if (service.equalsIgnoreCase("discord")) {
-            return ResponseEntity.ok(managerService.addDiscordService(user.getName(), serviceClient));
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Service not found");
-    }
-
-    @DeleteMapping("/profile/service/teamspeak/delete/{id}")
-    public ResponseEntity deleteUserTeamspeakService(@PathVariable int id, Authentication user, HttpServletRequest request) {
-
-        TeamspeakUser teamspeakUser = userService.getTeamspeakByID(id);
-
-        if (teamspeakUser == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The requested account was not found");
-        }
-        if (!teamspeakUser.getUsername().equalsIgnoreCase(user.getName()) && !request.isUserInRole(ADMIN_ROLE)) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You cannot delete service profile accounts that you don't own");
-        }
-        userService.deleteTeamspeakUUID(teamspeakUser);
-
-        return ResponseEntity.ok("Teamspeak account has been unregistered");
-    }
-
-    @DeleteMapping("/profile/service/discord/delete/{id}")
-    public ResponseEntity deleteUserDiscordService(@PathVariable int id, Authentication user, HttpServletRequest request) {
-        DiscordUser discordUser = userService.getDiscordById(id);
-
-        if (discordUser == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The requested account was not found");
-        }
-        if (!discordUser.getUsername().equalsIgnoreCase(user.getName()) && !request.isUserInRole(ADMIN_ROLE)) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You cannot delete service profile accounts that you don't own");
-        }
-        userService.deleteDiscordUUID(discordUser);
-
-        return ResponseEntity.ok("Discord account has been unregistered");
-    }
 }
