acc822a6f5cf374eb64f47051df7f372ca575507
Ryan <blackielifegfgaming@gmail.com>
2019-11-16 13:44:49 +0000

    Changed PasswordReset and Verification token
    PasswordReset and verification token now inherit from Token class to reduce code duplication

--- backend/src/main/java/biz/oneilindustries/website/entity/VerificationToken.java
+++ backend/src/main/java/biz/oneilindustries/website/entity/VerificationToken.java
@@ -1,99 +1,17 @@
 package biz.oneilindustries.website.entity;
 
-import javax.persistence.*;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Objects;
+import biz.oneilindustries.website.entity.supers.Token;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "verificationtoken")
-public class VerificationToken {
-    private static final int EXPIRATION = 60 * 24;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String token;
-
-    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
-    @JoinColumn(nullable = false, name = "username")
-    private User user;
-
-    @Column(name = "expiry_date")
-    private Date expiryDate;
-
-    private Date calculateExpiryDate() {
-        final Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(new java.util.Date().getTime());
-        cal.add(Calendar.MINUTE, EXPIRATION);
-        return new Date(cal.getTime().getTime());
-    }
+public class VerificationToken extends Token {
 
     public VerificationToken() {
     }
 
-    public VerificationToken(String token, User user, Date expiryDate) {
-        this.token = token;
-        this.user = user;
-        this.expiryDate = expiryDate;
-    }
-
-    public VerificationToken(String token, User user) {
-        this.token = token;
-        this.user = user;
-        this.expiryDate = calculateExpiryDate();
-    }
-
-    public static int getEXPIRATION() {
-        return EXPIRATION;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Date getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(Date expiryDate) {
-        this.expiryDate = expiryDate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        VerificationToken that = (VerificationToken) o;
-        return getId().equals(that.getId()) &&
-                getToken().equals(that.getToken()) &&
-                getUser().equals(that.getUser()) &&
-                getExpiryDate().equals(that.getExpiryDate());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getToken(), getUser(), getExpiryDate());
+    public VerificationToken(String token, User username) {
+        super(token, username);
     }
 }
