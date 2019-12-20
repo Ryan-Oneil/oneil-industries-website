1843af4e9e164c2fe99ce28c0551be74bfd57035
Ryan <blackielifegfgaming@gmail.com>
2019-12-20 21:42:07 +0000

    Changed footer and removed unused import

--- frontend/src/components/site layout/Footer.js
+++ frontend/src/components/site layout/Footer.js
@@ -1,16 +1,18 @@
 import React from "react";
-import '../../assets/css/layout.css';
-
+import "../../assets/css/layout.css";
 
 const Footer = () => {
   return (
-      <div className="bottomFooter ui lightBlack inverted vertical footer segment">
-          <div>
-              <h4 className="ui inverted centered header">
-                  &copy; Oneil Industries. All rights reserved.
-              </h4>
-          </div>
+    <div className="bottomFooter ui lightBlack inverted vertical footer segment">
+      <div>
+        <h4 className="ui inverted centered header">
+          &copy; Oneil Industries. All rights reserved. Created by{" "}
+          <a href="https://github.com/Ryan-Oneil/oneil-industries-website">
+            Ryan O'Neil
+          </a>
+        </h4>
       </div>
+    </div>
   );
 };
 
