630f993899088361a2680b1aa2962fd7a36cc365
Ryan-Oneil <44525616+Ryan-Oneil@users.noreply.github.com>
2021-05-06 21:39:50 +0100

    Updated user management ui

--- frontend/src/pages/admin/ManageUser.js
+++ frontend/src/pages/admin/ManageUser.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Button, Col, Modal, PageHeader, Row, Tabs } from "antd";
-import { Link } from "react-router-dom";
+import { Breadcrumb, Button, Col, PageHeader, Row, Tabs } from "antd";
 import { displayBytesInReadableForm } from "../../helpers";
 import StatisticCard from "../../components/Stats/StatisticCard";
 import {
@@ -12,14 +11,12 @@ import {
 import { getUserLinks } from "../../reducers/fileReducer";
 import EditUserForm from "../../components/formElements/EditUserForm";
 import SharedLinkTable from "../../components/Table/SharedLinkTable";
-import { USER_MEDIAS_ENDPOINT } from "../../apis/endpoints";
-import MediaGrid from "../../components/Gallery/MediaGrid";
-import { BASE_URL } from "../../apis/api";
-import Media from "../../components/Gallery/Media";
-import { deleteMedia } from "../../reducers/mediaReducer";
-import EditMediaForm from "../../components/formElements/EditMediaForm";
 import EditUserQuotaForm from "../../components/formElements/EditUserQuotaForm";
 import EditUserRole from "../../components/formElements/EditUserRole";
+import MediaModal from "../../components/Gallery/MediaModal";
+import ManageMediaGrid from "../../components/Gallery/ManageMediaGrid";
+import { Link } from "react-router-dom";
+import { ADMIN_BASE_URL } from "../../constants/constants";
 const { TabPane } = Tabs;
 
 export default props => {
@@ -58,11 +55,20 @@ export default props => {
 
   return (
     <div className="extraPadding">
-      <PageHeader
-        ghost={false}
-        onBack={() => window.history.back()}
-        title="Back"
-        extra={[
+      <Breadcrumb>
+        <Breadcrumb.Item>
+          <Link to={ADMIN_BASE_URL}>Dashboard</Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>
+          <Link to={`${ADMIN_BASE_URL}/users`}>Users</Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>{user}</Breadcrumb.Item>
+      </Breadcrumb>
+      <Tabs
+        defaultActiveKey="1"
+        size={"large"}
+        className={"userManagementNav"}
+        tabBarExtraContent={
           <Button
             key="1"
             type={`${account.enabled ? "danger" : "primary"}`}
@@ -72,103 +78,69 @@ export default props => {
           >
             {account.enabled ? "Disable Account" : "Enable Account"}
           </Button>
-        ]}
+        }
       >
-        <Tabs defaultActiveKey="1">
-          <TabPane tab="Account Settings" key="1">
-            <Row gutter={[32, 32]} type="flex">
-              <Col xs={24} sm={12} md={12} lg={12} xl={8}>
-                <EditUserForm user={account} loading={loadingUserDetails} />
-              </Col>
-              <Col xs={24} sm={12} md={12} lg={12} xl={8}>
-                <EditUserQuotaForm username={user} quota={account.quota} />
-              </Col>
-              <Col xs={24} sm={12} md={12} lg={12} xl={8}>
-                <EditUserRole role={account.role} username={user} />
-              </Col>
-            </Row>
-          </TabPane>
-          <TabPane tab="Files" key="2">
-            <SharedLinkTable
-              editPath={match.path}
-              fetchData={(page, size, sorter) =>
-                getUserLinks(user, page, size, sorter)
-              }
-            />
-          </TabPane>
-          <TabPane tab="Medias" key="3">
-            <MediaGrid
-              imageEndpoint={`${USER_MEDIAS_ENDPOINT}${user}/image`}
-              videoEndpoint={`${USER_MEDIAS_ENDPOINT}${user}/video`}
-              handleShowDialog={handleShowDialog}
-            />
-          </TabPane>
-          <TabPane tab="Stats" key="4">
-            <Row gutter={[32, 32]} type="flex" justify="center">
-              <Col xs={24} sm={24} md={5} lg={5} xl={5}>
-                <StatisticCard
-                  title={"Total Links"}
-                  value={totalLinks}
-                  loading={loadingFileStats}
-                />
-              </Col>
-              <Col xs={24} sm={24} md={5} lg={5} xl={5}>
-                <StatisticCard
-                  title={"Total Link Views"}
-                  value={totalViews}
-                  loading={loadingFileStats}
-                />
-              </Col>
-              <Col xs={24} sm={24} md={5} lg={5} xl={5}>
-                <StatisticCard
-                  title={"Total Used Storage"}
-                  value={displayBytesInReadableForm(account.quota.used)}
-                  loading={loadingUserDetails}
-                />
-              </Col>
-              <Col xs={24} sm={24} md={5} lg={5} xl={5}>
-                <StatisticCard title={"Total Media"} value={0} />
-              </Col>
-            </Row>
-          </TabPane>
-        </Tabs>
-      </PageHeader>
+        <TabPane tab="Account Settings" key="1">
+          <Row gutter={[32, 32]} type="flex">
+            <Col xs={24} sm={12} md={12} lg={12} xl={8}>
+              <EditUserForm user={account} loading={loadingUserDetails} />
+            </Col>
+            <Col xs={24} sm={12} md={12} lg={12} xl={8}>
+              <EditUserQuotaForm username={user} quota={account.quota} />
+            </Col>
+            <Col xs={24} sm={12} md={12} lg={12} xl={8}>
+              <EditUserRole role={account.role} username={user} />
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane tab="Files" key="2">
+          <SharedLinkTable
+            editPath={match.path}
+            fetchData={(page, size, sorter) =>
+              getUserLinks(user, page, size, sorter)
+            }
+          />
+        </TabPane>
+        <TabPane tab="Medias" key="3">
+          <ManageMediaGrid handleShowDialog={handleShowDialog} albums={[]} />
+        </TabPane>
+        <TabPane tab="Stats" key="4">
+          <Row gutter={[32, 32]} type="flex" justify="center">
+            <Col xs={24} sm={24} md={5} lg={5} xl={5}>
+              <StatisticCard
+                title={"Total Links"}
+                value={totalLinks}
+                loading={loadingFileStats}
+              />
+            </Col>
+            <Col xs={24} sm={24} md={5} lg={5} xl={5}>
+              <StatisticCard
+                title={"Total Link Views"}
+                value={totalViews}
+                loading={loadingFileStats}
+              />
+            </Col>
+            <Col xs={24} sm={24} md={5} lg={5} xl={5}>
+              <StatisticCard
+                title={"Total Used Storage"}
+                value={displayBytesInReadableForm(account.quota.used)}
+                loading={loadingUserDetails}
+              />
+            </Col>
+            <Col xs={24} sm={24} md={5} lg={5} xl={5}>
+              <StatisticCard title={"Total Media"} value={0} />
+            </Col>
+          </Row>
+        </TabPane>
+      </Tabs>
+      {/*</PageHeader>*/}
       {activeMedia && (
-        <Modal
-          title={activeMedia.name}
-          visible={activeMedia}
-          onCancel={() => setActiveMedia("")}
-          footer={null}
-          width={550}
-        >
-          <a
-            href={`${BASE_URL}/gallery/${activeMedia.mediaType}/${activeMedia.fileName}`}
-          >
-            <Media
-              fileName={activeMedia.fileName}
-              mediaType={activeMedia.mediaType}
-              fullSize={true}
-            />
-          </a>
-          <Button
-            value="Delete"
-            className="centerButton"
-            type="danger"
-            onClick={() => {
-              dispatch(
-                deleteMedia(
-                  `/gallery/media/delete/${activeMedia.id}`,
-                  activeMedia.id,
-                  activeMedia.size
-                )
-              );
-              setActiveMedia("");
-            }}
-          >
-            Delete
-          </Button>
-          <EditMediaForm media={activeMedia} />
-        </Modal>
+        <MediaModal
+          activeMedia={activeMedia}
+          closeModalAction={() => setActiveMedia("")}
+          showMediaPreview
+          enableManagement
+        />
       )}
     </div>
   );
